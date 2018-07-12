0e68e33f3e2012644d8c300ea01a6a85d84b98e2
Daniel Rivero <danielcfgm@gmail.com>
2018-07-12 20:49:38 +0200

    hey

---
+++ src/main/java/com/project/JDiscord/JDiscordApplication.java
@@ -0,0 +1,12 @@
+package com.project.JDiscord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDiscordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDiscordApplication.class, args);
+	}
+}
