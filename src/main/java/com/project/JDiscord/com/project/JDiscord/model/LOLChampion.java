f7761ce7bb48bf98c77104d0960353b864f92a53
Daniel Rivero <danielcfgm@gmail.com>
2018-07-12 21:50:34 +0200

    Lombok changes added

--- src/main/java/com/project/JDiscord/com/project/JDiscord/model/LOLChampion.java
+++ src/main/java/com/project/JDiscord/com/project/JDiscord/model/LOLChampion.java
@@ -1,12 +1,16 @@
 package com.project.JDiscord.com.project.JDiscord.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of={"name"})
 public class LOLChampion {
 
     private String name;
-
-    public LOLChampion(){
-
-    }
-
+    
 }
